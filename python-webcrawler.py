a99358bbf02b8460e22af6943889d80d2f34086e
Richiie89 <62986086+Richiie89@users.noreply.github.com>
2020-04-24 16:50:17 +0200

    Add files via upload
    Beginning of porting my bash written web-crawler to Python. Needs some modification in order to work

---
+++ python-webcrawler.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import threading
+import socket
+import subprocess
+import sys
+import os
+import nmap
+from datetime import datetime
+#Clear the screen#
+subprocess.call('clear', shell=True)
+#
+#
+print "RUNNING STAGE1 (port scanning, this will take a while)"
+with open(whois-netherlands-full.txt) as f:
+for line in f:
+print(line)
